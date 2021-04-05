c82ca76216548d2a383890764a8fca7d03f04b36
miguebeto <miguelangell1994@hotmail.com>
2021-04-05 11:54:50 -0500

    creado Endpoint con get y parametros en API

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const { text } = require('express');
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.port || 3001
@@ -10,7 +10,9 @@ const port = process.env.port || 3001
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-
+app.get('/hola/:name', (req, res)=>{
+    res.send({ message : `Hola ${req.params.name}!`});
+});
 app.listen(port, ()=>{
     console.log(`Api rest corriendo en http://localhost:${port}`);
 });
