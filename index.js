a72d8441846c437103d1a25181c68a9ca849cb21
miguebeto <miguelangell1994@hotmail.com>
2021-04-05 16:01:05 -0500

    Adicionado metodo get y post con postman

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict'
+/* const http = require('http');
+http.createServer(function(req, res){
+    res.writeHead(200,{'content-type': 'text/html'});
+    res.end('Hello World!');
+}).listen(8080); */
+
 
 const { text } = require('express');
 const express = require('express');
@@ -10,16 +16,29 @@ const port = process.env.port || 3001
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/hola/:name', (req, res)=>{
-    res.send({ message : `Hola ${req.params.name}!`});
+app.get('/api/product', (req,res)=>{
+res.send(200, {products: []});
 });
+
+app.get('/api/:product', (req,res)=>{
+
+});
+
+app.post('/api/product', (req,res)=>{
+console.log(req.body);
+res.status(200).send({massage: 'El producto se ha recibido'});
+});
+
+app.put('/api/product', (req,res)=>{
+
+});
+
+app.delete('/api/product', (req,res)=>{
+
+});
+
 app.listen(port, ()=>{
     console.log(`Api rest corriendo en http://localhost:${port}`);
 });
 
 
-/* const http = require('http');
-http.createServer(function(req, res){
-    res.writeHead(200,{'content-type': 'text/html'});
-    res.end('Hello World!');
-}).listen(8080); */
