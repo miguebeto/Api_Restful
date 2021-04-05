a691bbe9c3c0cc02896efaa59e0163acacbeb345
miguebeto <miguelangell1994@hotmail.com>
2021-04-05 09:06:32 -0500

    Instalando y configurando nodemon

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const express = require('express');
 const bodyParser = require('body-parser')
 
 const app = express();
-const port = process.env.port || 3000
+const port = process.env.port || 3001
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log(`Api rest corriendo en http://localhost:${port}`);
 });
 
